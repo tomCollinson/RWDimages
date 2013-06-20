79f0bff7d11393fff6ea4472c32245a5431ccd1c
tomCollinson <tom@tom-collinson.com>
2013-06-20 15:59:46 +0200

    Update rwdimages.jquery.js
    Refactoring

--- rwdimages.jquery.js
+++ rwdimages.jquery.js
@@ -1,45 +1,56 @@
+
 (function($) {
-  $.fn.responsive = function( options ) {
-    // Default Settings
+	$.fn.responsive = function( options ) {
+		
 		var settings = $.extend({
-			imgClass	:	'img-responsive',
+			imgClass  :  '.img-responsive',
+			containerClass : '.rwdimg',
+			fadeLoad : false,
 			mobile : 360,
-			tablet : 780,
+			tablet : 620,
 			desktop	: 900
 		}, options);
 		
+		function newUrl (breakpoint){
+				var src = $(settings.imgClass).data('src');
+				var path = src.substring(0,src.lastIndexOf('/')); 
+				var fileName = src.substring(src.lastIndexOf('/'));	
+				var fileExtn = src.substring(src.lastIndexOf('.'));
+				var fileNameReplace = src.substring(src.lastIndexOf('_'));
+				var newSrc = path+fileName.replace(fileNameReplace, "_" + breakpoint + fileExtn);	
+				return newSrc;
+		}
+		
 		function updateUrl (breakpoint){ 
 			$(settings.imgClass).each(function(){
-			var src = $(settings.imgClass).data('src');
-			var path = src.substring(0,src.lastIndexOf('/'));
-			var fileName = src.substring(src.lastIndexOf('/'));
-			var fileExtn = src.substring(src.lastIndexOf('.'));
-			var fileNameReplace = src.substring(src.lastIndexOf('_'));
-			var newSrc = path+fileName.replace(fileNameReplace, "_" + breakpoint + fileExtn);	// build the new source string
-			console.log(newSrc);
-			$(this).closest('.media').html("<img src='" + newSrc + "'  class='img-replace' style='visibility:hidden'/>");
-			$('.img-replace').load(function(){
-				$('.img-replace').hide().css({visibility: "visible"}).fadeIn("slow");
-			});
+			
+				var newSrc = newUrl(breakpoint);
+				$(this).closest(settings.containerClass).html("<img src='" + newSrc + "'  class='rwdimg-replaced' />");
+				
+				// If the user has set the images to fade in once loaded
+				if (settings.fadeLoad) {
+					$('.rwdimg-replace').load(function(){
+						$('.rwdimg-replace').hide().css({visibility: "visible"}).fadeIn("slow");
+					});
+				} // end if
 			
 			});
 		}
+		
 		function changeMedia (){
-		/*
-			changeMedia() identifies the current window width, compares it to
-			the user-set breakpoints and calls updateURL(), supplying it with the
-			breakpoint it should work with.
-		*/
-		if ($(window).width() < settings.tablet){
-					updateUrl(settings.mobile);
-				};
-		if ($(window).width() >settings.tablet && $(window).width() < settings.desktop){
-					updateUrl(settings.tablet);
-				};
-		if ($(window).width() >= settings.desktop){
-					updateUrl(settings.desktop);
-				};
+			var width = $(window).width();
+			if (width < settings.tablet){
+						updateUrl(settings.mobile);
+					};
+			if (width >= settings.tablet && width < settings.desktop){
+						updateUrl(settings.tablet);
+					};
+			if (width >= settings.desktop){
+						updateUrl(settings.desktop);
+					};
 		}	
+
+		// Run script when the page is loaded
 		$(window).on("load", function(){
 				changeMedia();
 		}); 
